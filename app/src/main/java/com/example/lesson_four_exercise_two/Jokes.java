97a4330b060a5c0fd7fbd62c138dc0a3cacc004c
MaryWindows <anne.maggot@gmail.com>
2021-11-18 08:09:50 +0200

    Jokes Page

---
+++ app/src/main/java/com/example/lesson_four_exercise_two/Jokes.java
@@ -0,0 +1,14 @@
+package com.example.lesson_four_exercise_two;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Jokes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
+    }
+}
